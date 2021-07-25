2326e530f487069ee6acc487fa1da82fad6aec71
Luke <kingluke2222@live.com>
2021-07-25 15:03:39 -0700

    add changing turns and check for wins

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,11 @@
 const O = 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/LetterO.svg/1200px-LetterO.svg.png)';
 const X = 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Letter_x.svg/1200px-Letter_x.svg.png)';
 
+const player = (symbol) => {
+    const tilesTaken = [];
+    return {symbol,tilesTaken};
+};
+
 const gameBoard = (() => {
     const tiles = document.getElementsByClassName('tile');
     
@@ -18,16 +23,72 @@ const gameBoard = (() => {
 
 const gameController = (() =>{
 
+    const player1 = player(X);
+    const player2 = player(O);
+    this.currentPlayer = player1;
+
+    const changeTurn = () => {
+        if (currentPlayer == player1){
+            currentPlayer = player2;
+        } else{
+            currentPlayer = player1;
+        }
+    }
+
+    const PlayTurn = () => {
+
+        gameBoard.getTiles().forEach(element => {
+            element.addEventListener('click', (element) => {
+                element.target.style.backgroundImage = this.currentPlayer.symbol;
+                this.currentPlayer.tilesTaken.push(element.target);
+                console.log('Have we won: ', checkWin(this.currentPlayer.tilesTaken));
+                changeTurn();
+            })
+        });
+    }
+
+    const checkWin = (playersTiles) =>{
+        let ourTiles = '';
+        playersTiles.forEach(element => {
+            ourTiles = ourTiles.concat(element.className);
+        })
+
+        if (ourTiles.includes('t1') && ourTiles.includes('t2') &&  ourTiles.includes('t3')){
+            return true
+        } 
+        else if (ourTiles.includes('t4') && ourTiles.includes('t5') &&  ourTiles.includes('t6')){
+            return true
+        }
+        else if (ourTiles.includes('t4') && ourTiles.includes('t5') &&  ourTiles.includes('t6')){
+            return true; 
+        }
+        else if (ourTiles.includes('t7') && ourTiles.includes('t8') &&  ourTiles.includes('t9')){
+            return true; 
+        }
+        else if (ourTiles.includes('t1') && ourTiles.includes('t4') &&  ourTiles.includes('t7')){
+            return true; 
+        }
+        else if (ourTiles.includes('t2') && ourTiles.includes('t5') &&  ourTiles.includes('t8')){
+            return true; 
+        }
+        else if (ourTiles.includes('t3') && ourTiles.includes('t6') &&  ourTiles.includes('t9')){
+            return true; 
+        }
+        else if (ourTiles.includes('t3') && ourTiles.includes('t5') &&  ourTiles.includes('t7')){
+            return true; 
+        }
+        else if (ourTiles.includes('t1') && ourTiles.includes('t5') &&  ourTiles.includes('t9')){
+            return true; 
+        } else{
+            return false; 
+
+        }
+
+    }
+
+    return {PlayTurn};
 })();
 
-const player = (symbol) => {
-    let playerSymbol = symbol;
-    return {symbol};
-};
 
 
-gameBoard.getTiles().forEach(element => {
-    element.addEventListener('click', (element, gameController.currentPlayer) => {
-        element.target.style.backgroundImage = currentPlayer.playerSymbol;
-    })
-});
+gameController.PlayTurn();
